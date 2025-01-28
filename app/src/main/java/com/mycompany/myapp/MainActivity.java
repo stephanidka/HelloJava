708f0f79d55f08fb5b4a8f73d676a00446c3399b
Stepanida <stephany.vakhotina@gmail.com>
2025-01-28 23:49:03 +0100

    gradle

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.mycompany.myapp;
 
-import android.app.*;
-import android.os.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main); // Связываем с XML
+
+        // Находим кнопку по её ID
+        Button myButton = findViewById(R.id.myButton);
+
+        // Устанавливаем обработчик нажатия
+        myButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Меняем цвет кнопки
+					myButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+					myButton.setText("Цвет изменён!");
+				}
+			});
     }
 }
+
